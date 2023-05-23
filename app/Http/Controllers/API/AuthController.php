2e2274d98350b9991b37bdfcf5297898717a35c5
DimasBagus0 <dmsadityas@gmail.com>
2023-05-23 08:36:49 +0700

    first commit

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator as FacadesValidator;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Facade;
+
+class AuthController extends Controller
+{
+
+    public function register(Request $request)
+    {
+        $validator = FacadesValidator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required|numeric|min:15',
+            'password' => 'required',
+            'confirm_password' => 'required|same:password'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Ada kesalahan',
+                'data' => $validator->errors()
+            ]);
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+
+        $success['token'] = $user->createToken('auth_token')->plainTextToken;
+        $success['name'] = $user->name;
+        $success['email'] = $user->email;
+        $success['phone'] =$user->phone;
+
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Sukses register',
+            'data' => $success
+        ]);
+
+    }
+
+    public function login(Request $request)
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password]))
+        {
+            $auth = Auth::user();
+            $success['token'] = $auth->createToken('auth_token')->plainTextToken;
+            $success['name'] = $auth->name;
+            $success['email'] = $auth->email;
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Login sukses',
+                'data' => $success
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Cek email dan password lagi',
+                'data' => null
+            ]);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json([
+            'message' => 'Logout succes'
+        ]);
+    }
+
+}
